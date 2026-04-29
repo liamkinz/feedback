3676650682b80e224f5acdd336aedd39d9cbb284
Liam <liamkinazo12@gmail.com>
2026-04-29 19:46:56 +0800

    first commit

---
+++ src/components/Testings.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// no script needed yet
+</script>
+
+<template>
+  <v-app>
+    <v-app-bar color="primary" dark>
+      <v-app-bar-title>My Vuetify App</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-btn color="primary">Click Me</v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { vuetify } from './plugins/vuetify'
+
+import App from './App.vue'
+import router from './router'
+
+// Icons from Vuetify
+import '@mdi/font/css/materialdesignicons.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(vuetify)
+
+app.mount('#app')
