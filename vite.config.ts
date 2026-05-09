219b7536c1b81aa8b81e9a18e2f51ba952c57e29
Liam <liamkinazo12@gmail.com>
2026-05-09 16:10:13 +0800

    feat: add PWA support and update dependencies
    - Added @mdi/js and @supabase/supabase-js to dependencies in package.json.
    - Introduced vite-plugin-pwa for Progressive Web App support in vite.config.ts, including caching strategies and manifest configuration.
    - Removed useFeedback composable as it is no longer needed.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import vuetify from 'vite-plugin-vuetify'
 import ViteFonts from 'unplugin-fonts/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,11 +13,55 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     vuetify({ autoImport: true }),
-    // ViteFonts({
-    //   google: {
-    //     families: ['Roboto'],
-    //   },
-    // }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+        suppressWarnings: true,
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2}'],
+        navigateFallback: 'index.html',
+        runtimeCaching: [
+          {
+            // cache Google Fonts if you use them
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 },
+            },
+          },
+        ],
+      },
+      manifest: {
+        name: 'CSMR App',
+        short_name: 'Feedback',
+        description: 'A CSMR app built with Vue 3 and Vite',
+        theme_color: '#4A90E2',
+        background_color: '#FFFFFF',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/src/assets/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/src/assets/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/src/assets/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable', // for Android adaptive icons
+          },
+        ],
+      },
+    }),
     ViteFonts({
       fontsource: {
         families: [
