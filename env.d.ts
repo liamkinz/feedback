3125ce7e9ea03828390dd4eabe57db9b2bd5d4ee
Liam <liamkinazo12@gmail.com>
2026-05-09 16:09:35 +0800

    Refactor code structure for improved readability and maintainability

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="vite/client" />
 
+/// <reference types="vite-plugin-pwa/client" />
+
 // declare module '*.vue' {
 //   import type { DefineComponent } from 'vue'
 //   const component: DefineComponent<object, object, object>
