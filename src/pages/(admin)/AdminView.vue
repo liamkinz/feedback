311536d00d803bdf1faf1c74108398fa46434fb8
Liam <liamkinazo12@gmail.com>
2026-05-28 21:53:46 +0800

    fix: correct import name for FinalInspectChart component in AdminView

--- src/pages/(admin)/AdminView.vue
+++ src/pages/(admin)/AdminView.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import SiteInspectionChart from '@/pages/(admin)/components/SiteInspectionChart.vue'
-import FinalInspectionChart from '@/pages/(admin)/components/FinalInspectionChart.vue'
+import FinalInspectChart from '@/pages/(admin)/components/FinalInspectChart.vue'
 import AnnualInspectionChart from '@/pages/(admin)/components/AnnualInspectionChart.vue'
 </script>
 
 <template>
   <div class="d-flex flex-column">
     <SiteInspectionChart class="mb-8" />
-    <FinalInspectionChart class="mb-8" />
+    <FinalInspectChart class="mb-8" />
     <AnnualInspectionChart class="mb-8" />
   </div>
 </template>
